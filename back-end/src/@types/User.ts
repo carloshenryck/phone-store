8743bce86f8bbead046f330333b819ff1d192ac5
Carlos Henryck <ryck302@gmail.com>
2024-03-05 03:07:31 -0300

    refactor: move schemas to folder schemas

--- back-end/src/@types/User.ts
+++ back-end/src/@types/User.ts
@@ -1,15 +1,5 @@
 import { z } from "zod";
-
-export const loginSchema = z.object({
-  email: z.string({required_error: 'O email é obrigatório'}).email('O email passado não é válido'),
-  password: z.string({required_error: 'A senha é obrigatória'}).min(6, 'A senha precisa conter pelo menos seis caracteres')
-})
-
-export const registerSchema = z.object({
-  name: z.string({required_error: 'O nome é obrigatório'}).max(10, 'O nome pode conter no máximo dez caracteres'),
-  email: z.string({required_error: 'O email é obrigatório'}).email('O email passado não é válido'),
-  password: z.string({required_error: 'A senha é obrigatória'}).min(6, 'A senha precisa conter pelo menos seis caracteres')
-})
+import { loginSchema, registerSchema } from "../schemas/user.schema";
 
 export type IUserForLogin = z.infer<typeof loginSchema>
 export type IUserForRegister = z.infer<typeof registerSchema>
