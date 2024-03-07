81574372275ccf8aa32390c2e9582dff4fa10f39
Carlos Henryck <ryck302@gmail.com>
2024-03-07 03:44:41 -0300

    feat: add empty state to user phones page

--- front-end/src/pages/UserPhones/index.tsx
+++ front-end/src/pages/UserPhones/index.tsx
@@ -5,8 +5,10 @@ import { Phone } from "@/@types/Phone";
 import Spinner from "@/components/Spinner";
 import PhoneCard from "@/components/PhoneCard";
 import { usePhoneStore } from "@/stores/PhoneStore";
+import { useNavigate } from "react-router-dom";
 
 export default function UserPhones() {
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const { phones, setPhones } = usePhoneStore();
 
@@ -30,11 +32,23 @@ export default function UserPhones() {
           <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             <Spinner className="h-16 w-16 border-[6px]" />
           </div>
-        ) : (
+        ) : phones.length > 0 ? (
           <div className="grid grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-4">
             {phones.map((phone) => (
               <PhoneCard key={phone.id} {...phone} isAuthor />
             ))}
+          </div>
+        ) : (
+          <div>
+            <p className="text-xl text-zinc-600">
+              Você não tem nenhum produto à venda
+            </p>
+            <button
+              onClick={() => navigate("/home")}
+              className="border-[2px] border-solid border-orange-300 p-2 rounded-lg mt-4 text-orange-400"
+            >
+              Voltar pra tela inicial
+            </button>
           </div>
         )}
       </div>
